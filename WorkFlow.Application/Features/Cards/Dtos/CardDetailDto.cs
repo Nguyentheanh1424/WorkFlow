2de8879b9dff569d132ab939fa6379f762601261
nthea <ntheanh0104@gmail.com>
2026-01-02 10:47:05 +0700

    fix api

--- WorkFlow.Application/Features/Cards/Dtos/CardDetailDto.cs
+++ WorkFlow.Application/Features/Cards/Dtos/CardDetailDto.cs
@@ -14,7 +14,7 @@ namespace WorkFlow.Application.Features.Cards.Dtos
         public string? Description { get; set; }
         public int Position { get; set; }
 
-        public string? Label { get; set; }
+        public int[]? Label { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? DueDate { get; set; }
         public bool ReminderEnabled { get; set; }
