d426df68f0d199d266c98d37d7a21f11b172f8d9
nthea <ntheanh0104@gmail.com>
2026-01-03 14:32:53 +0700

    hotfix logic delete workspace

--- WorkFlow.Application/Features/WorkSpaces/Commands/DeleteWorkspaceCommand.cs
+++ WorkFlow.Application/Features/WorkSpaces/Commands/DeleteWorkspaceCommand.cs
@@ -1,6 +1,10 @@
-using FluentValidation;
+﻿using FluentValidation;
 using MediatR;
+using WorkFlow.Application.Common.Interfaces.Auth;
+using WorkFlow.Application.Common.Interfaces.Repositories;
+using WorkFlow.Application.Common.Interfaces.Services;
 using WorkFlow.Domain.Common;
+using WorkFlow.Domain.Entities;
 
 namespace WorkFlow.Application.Features.WorkSpaces.Commands
 {
@@ -11,6 +15,41 @@ namespace WorkFlow.Application.Features.WorkSpaces.Commands
         public DeleteWorkspaceCommandValidator()
         {
             RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+
+    public class DeleteWorkspaceCommandHandler : IRequestHandler<DeleteWorkspaceCommand, Result<bool>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<WorkSpace, Guid> _repository;
+        private readonly ICurrentUserService _currentUser;
+        private readonly IPermissionService _permission;
+
+        public DeleteWorkspaceCommandHandler(IUnitOfWork unitOfWork, ICurrentUserService currentUserService, IPermissionService permissionService)
+        {
+            _unitOfWork = unitOfWork;
+            _permission = permissionService;
+            _currentUser = currentUserService;
+            _repository = _unitOfWork.GetRepository<WorkSpace, Guid>();
+        }
+
+        public async Task<Result<bool>> Handle(DeleteWorkspaceCommand request, CancellationToken cancellationToken)
+        {
+            if (_currentUser.UserId == null)
+                return Result<bool>.Failure("Không xác định được người dùng.");
+
+            var userId = _currentUser.UserId.Value;
+
+            var workSpace = await _repository.GetByIdAsync(request.Id);
+            if (workSpace == null)
+                return Result<bool>.Failure("Workspace không tồn tại.");
+
+            await _permission.Workspace.EnsureOwnerAsync(workSpace.Id, userId);
+
+            await _repository.DeleteAsync(workSpace);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result<bool>.Success(true);
         }
     }
 }
